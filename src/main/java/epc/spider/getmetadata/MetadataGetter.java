0a493fa80492ecdd13c834f191a9b956bc4a8dec
olov <olov.mckie@ub.uu.se>
2015-02-20 08:32:47 +0100

    Adding some documentation

--- src/main/java/epc/spider/getmetadata/MetadataGetter.java
+++ src/main/java/epc/spider/getmetadata/MetadataGetter.java
@@ -3,20 +3,41 @@ package epc.spider.getmetadata;
 import epc.metadataformat.CoherentMetadata;
 import epc.metadataformat.getmetadata.MetadataStorageGetterInputBoundry;
 
+/**
+ * MetadataGetter makes it possible to get metadata from inner parts of the
+ * system.
+ * 
+ * @author <a href="mailto:olov.mckie@ub.uu.se">Olov McKie</a>
+ *
+ * @since 0.1
+ *
+ */
 public class MetadataGetter implements MetadataGetterInputBoundry {
 
-	private MetadataStorageGetterInputBoundry getMetadataInputBoundry;
+	private final MetadataStorageGetterInputBoundry getMetadataInputBoundry;
 
-	public MetadataGetter(MetadataStorageGetterInputBoundry getMetadataInputBoundry) {
-		if (null == getMetadataInputBoundry) {
+	/**
+	 * MetadataGetter initializes this MetadataGetter so that it uses the
+	 * provided MetadataStorageGetterInputBoundry to get metadata.
+	 * 
+	 * @param metadataStorageGetterInputBoundry
+	 *            An implementation of the
+	 *            {@link MetadataStorageGetterInputBoundry} interface to provide
+	 *            implementation for the getter.
+	 */
+	public MetadataGetter(
+			MetadataStorageGetterInputBoundry metadataStorageGetterInputBoundry) {
+		if (null == metadataStorageGetterInputBoundry) {
 			throw new IllegalArgumentException(
 					"getMetadataInputBoundry must not be null");
 		}
-		this.getMetadataInputBoundry = getMetadataInputBoundry;
+		this.getMetadataInputBoundry = metadataStorageGetterInputBoundry;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public CoherentMetadata getAllMetadata() {
-
 		return getMetadataInputBoundry.getAllMetadata();
 	}
 
--- src/main/java/epc/spider/getmetadata/MetadataGetterInputBoundry.java
+++ src/main/java/epc/spider/getmetadata/MetadataGetterInputBoundry.java
@@ -2,6 +2,21 @@ package epc.spider.getmetadata;
 
 import epc.metadataformat.CoherentMetadata;
 
+/**
+ * MetadataGetterInputBoundry provides an interface for the MetadataGetter
+ * 
+ * @author <a href="mailto:olov.mckie@ub.uu.se">Olov McKie</a>
+ *
+ * @since 0.1
+ *
+ */
 public interface MetadataGetterInputBoundry {
+	/**
+	 * getAllMetadata returns all metadata for the whole system, as a
+	 * CoherentMetadata populated with metadataFormat, Presentation, Collections
+	 * and Texts
+	 * 
+	 * @return A CoherentMetadata with all metadata for the entire system
+	 */
 	public CoherentMetadata getAllMetadata();
 }
