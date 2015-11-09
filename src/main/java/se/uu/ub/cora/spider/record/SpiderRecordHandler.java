497eab081a2ffff2cdc957d7378130dfac1d3f4b
olov <olov.mckie@ub.uu.se>
2015-11-09 12:23:02 +0100

    CORA-110 add action read for DataRecordLinks when reading data as list

--- src/main/java/se/uu/ub/cora/spider/record/SpiderRecordHandler.java
+++ src/main/java/se/uu/ub/cora/spider/record/SpiderRecordHandler.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2015 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package se.uu.ub.cora.spider.record;
 
 import se.uu.ub.cora.bookkeeper.data.DataGroup;
@@ -18,24 +37,10 @@ public class SpiderRecordHandler {
 		return recordStorage.read(RECORD_TYPE, recordType);
 	}
 
-	protected void addLinks(SpiderDataRecord spiderDataRecord) {
-		spiderDataRecord.addAction(Action.READ);
-		spiderDataRecord.addAction(Action.UPDATE);
-		spiderDataRecord.addAction(Action.DELETE);
-		if (incomingLinksExistsForRecord()) {
-			spiderDataRecord.addAction(Action.READ_INCOMING_LINKS);
-		}
-	}
-
-	protected boolean incomingLinksExistsForRecord() {
-		return recordStorage.linksExistForRecord(recordType, recordId);
-	}
-
 	protected void addReadActionToDataRecordLinks(SpiderDataGroup spiderDataGroup) {
 		for (SpiderDataElement spiderDataChild : spiderDataGroup.getChildren()) {
 			addReadActionToDataRecordLink(spiderDataChild);
 		}
-
 	}
 
 	private void addReadActionToDataRecordLink(SpiderDataElement spiderDataChild) {
@@ -61,5 +66,22 @@ public class SpiderRecordHandler {
 		SpiderDataRecord spiderDataRecord = SpiderDataRecord.withSpiderDataGroup(spiderDataGroup);
 		addLinks(spiderDataRecord);
 		return spiderDataRecord;
+	}
+
+	protected void addLinks(SpiderDataRecord spiderDataRecord) {
+		spiderDataRecord.addAction(Action.READ);
+		spiderDataRecord.addAction(Action.UPDATE);
+		spiderDataRecord.addAction(Action.DELETE);
+		if (incomingLinksExistsForRecord(spiderDataRecord)) {
+			spiderDataRecord.addAction(Action.READ_INCOMING_LINKS);
+		}
+	}
+
+	protected boolean incomingLinksExistsForRecord(SpiderDataRecord spiderDataRecord) {
+		SpiderDataGroup spiderDataGroup = spiderDataRecord.getSpiderDataGroup();
+		SpiderDataGroup recordInfo = spiderDataGroup.extractGroup("recordInfo");
+		String recordType = recordInfo.extractAtomicValue("type");
+		String recordId = recordInfo.extractAtomicValue("id");
+		return recordStorage.linksExistForRecord(recordType, recordId);
 	}
 }
