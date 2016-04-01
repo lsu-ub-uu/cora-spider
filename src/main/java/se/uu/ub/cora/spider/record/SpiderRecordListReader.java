85a94d46613c467e0c9a2bd8a01c0ce9126a8008
madeleine <madeleine.kennback@ub.uu.se>
2016-04-01 12:06:40 +0200

    CORA-174, added action createByUpload

--- src/main/java/se/uu/ub/cora/spider/record/SpiderRecordListReader.java
+++ src/main/java/se/uu/ub/cora/spider/record/SpiderRecordListReader.java
@@ -21,12 +21,6 @@ package se.uu.ub.cora.spider.record;
 
 import se.uu.ub.cora.spider.data.SpiderDataList;
 
-/**
- * TODO: Class description
- *
- * @author <a href="mailto:madeleine.kennback@ub.uu.se">Madeleine Kennbäck</a>
- * @version $Revision$, $Date$, $Author$
- */
 public interface SpiderRecordListReader {
     SpiderDataList readRecordList(String userId, String type);
 }
