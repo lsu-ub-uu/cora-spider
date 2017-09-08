7f25e0a6b3f936fefb7a9625b69ed74ed49755d9
madde <madde@f7176c87b7ff>
2017-09-08 09:01:02 +0000

    Moving readIncomingLinks to a class of its own

--- src/main/java/se/uu/ub/cora/spider/record/SpiderRecordReaderImp.java
+++ src/main/java/se/uu/ub/cora/spider/record/SpiderRecordReaderImp.java
@@ -19,18 +19,11 @@
 
 package se.uu.ub.cora.spider.record;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import se.uu.ub.cora.beefeater.authentication.User;
 import se.uu.ub.cora.bookkeeper.data.DataGroup;
 import se.uu.ub.cora.spider.authentication.Authenticator;
 import se.uu.ub.cora.spider.authorization.SpiderAuthorizator;
-import se.uu.ub.cora.spider.data.Action;
-import se.uu.ub.cora.spider.data.SpiderDataGroup;
-import se.uu.ub.cora.spider.data.SpiderDataList;
 import se.uu.ub.cora.spider.data.SpiderDataRecord;
-import se.uu.ub.cora.spider.data.SpiderDataRecordLink;
 import se.uu.ub.cora.spider.dependency.SpiderDependencyProvider;
 
 public final class SpiderRecordReaderImp extends SpiderRecordHandler implements SpiderRecordReader {
@@ -40,7 +33,6 @@ public final class SpiderRecordReaderImp extends SpiderRecordHandler implements 
 	private SpiderAuthorizator spiderAuthorizator;
 	private User user;
 	private String authToken;
-	private RecordTypeHandler recordTypeHandler;
 
 	private SpiderRecordReaderImp(SpiderDependencyProvider dependencyProvider,
 			DataGroupToRecordEnhancer dataGroupToRecordEnhancer) {
@@ -63,8 +55,6 @@ public final class SpiderRecordReaderImp extends SpiderRecordHandler implements 
 		this.authToken = authToken;
 		this.recordType = recordType;
 		this.recordId = recordId;
-		recordTypeHandler = RecordTypeHandler.usingRecordStorageAndRecordTypeId(recordStorage,
-				recordType);
 		tryToGetActiveUser();
 		checkUserIsAuthorizedForActionOnRecordType();
 		DataGroup recordRead = recordStorage.read(recordType, recordId);
@@ -85,96 +75,8 @@ public final class SpiderRecordReaderImp extends SpiderRecordHandler implements 
 		spiderAuthorizator.checkUserIsAuthorizedForActionOnRecordType(user, READ, recordType);
 	}
 
-	private void checkRecordsRecordTypeNotAbstract() {
-		if (recordTypeHandler.isAbstract()) {
-			throw new MisuseException("Reading for record: " + recordId
-					+ " on the abstract recordType:" + recordType + " is not allowed");
-		}
-	}
-
 	private void checkUserIsAuthorisedToReadData(DataGroup recordRead) {
 		spiderAuthorizator.checkUserIsAuthorizedForActionOnRecordTypeAndRecord(user, READ,
 				recordType, recordRead);
-	}
-
-	@Override
-	public SpiderDataList readIncomingLinks(String authToken, String recordType, String recordId) {
-		// TODO: break out this method
-		this.authToken = authToken;
-		this.recordType = recordType;
-		this.recordId = recordId;
-		recordTypeHandler = RecordTypeHandler.usingRecordStorageAndRecordTypeId(recordStorage,
-				recordType);
-		tryToGetActiveUser();
-		checkRecordsRecordTypeNotAbstract();
-		DataGroup recordRead = recordStorage.read(recordType, recordId);
-
-		checkUserIsAuthorisedToReadData(recordRead);
-
-		return collectLinksAndConvertToSpiderDataList();
-	}
-
-	private SpiderDataList collectLinksAndConvertToSpiderDataList() {
-		Collection<DataGroup> links = new ArrayList<>();
-		addLinksPointingToRecord(links);
-		possiblyAddLinksPointingToRecordByParentRecordType(links);
-
-		return convertLinksPointingToRecordToSpiderDataList(links);
-	}
-
-	private void addLinksPointingToRecord(Collection<DataGroup> links) {
-		Collection<DataGroup> linksPointingToRecord = recordStorage
-				.generateLinkCollectionPointingToRecord(recordType, recordId);
-		links.addAll(linksPointingToRecord);
-	}
-
-	private void possiblyAddLinksPointingToRecordByParentRecordType(Collection<DataGroup> links) {
-		DataGroup recordTypeDataGroup = getRecordTypeDefinition();
-		if(recordTypeHasParent(recordTypeDataGroup)){
-			String parentId = extractParentId(recordTypeDataGroup);
-			Collection<DataGroup> linksPointingToParentType = recordStorage.generateLinkCollectionPointingToRecord(parentId, recordId);
-			links.addAll(linksPointingToParentType);
-		}
-	}
-
-	private boolean recordTypeHasParent(DataGroup recordTypeDataGroup) {
-		return recordTypeDataGroup.containsChildWithNameInData("parentId");
-	}
-
-	private String extractParentId(DataGroup recordTypeDataGroup) {
-		DataGroup parent = recordTypeDataGroup.getFirstGroupWithNameInData("parentId");
-		return parent.getFirstAtomicValueWithNameInData("linkedRecordId");
-	}
-
-	private SpiderDataList convertLinksPointingToRecordToSpiderDataList(
-			Collection<DataGroup> dataGroupLinks) {
-		SpiderDataList recordToRecordLinkList = createDataListForRecordToRecordLinks(
-				dataGroupLinks);
-		convertAndAddLinksToLinkList(dataGroupLinks, recordToRecordLinkList);
-		return recordToRecordLinkList;
-	}
-
-	private SpiderDataList createDataListForRecordToRecordLinks(Collection<DataGroup> links) {
-		SpiderDataList recordToRecordList = SpiderDataList
-				.withContainDataOfType("recordToRecordLink");
-		recordToRecordList.setFromNo("1");
-		recordToRecordList.setToNo(Integer.toString(links.size()));
-		recordToRecordList.setTotalNo(Integer.toString(links.size()));
-		return recordToRecordList;
-	}
-
-	private void convertAndAddLinksToLinkList(Collection<DataGroup> links,
-			SpiderDataList recordToRecordList) {
-		for (DataGroup dataGroup : links) {
-			SpiderDataGroup spiderDataGroup = SpiderDataGroup.fromDataGroup(dataGroup);
-			addReadActionToIncomingLinks(spiderDataGroup);
-			recordToRecordList.addData(spiderDataGroup);
-		}
-	}
-
-	private void addReadActionToIncomingLinks(SpiderDataGroup spiderDataGroup) {
-		SpiderDataRecordLink spiderRecordLink = (SpiderDataRecordLink) spiderDataGroup
-				.getFirstChildWithNameInData("from");
-		spiderRecordLink.addAction(Action.READ);
 	}
 }
