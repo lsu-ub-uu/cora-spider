7afaf19b0bc56692a333bc5a824ad554c8809550
madde <madde@60b566aecc84>
2018-04-27 18:44:52 +0000

    CORA-581, fixed test for duplicate id, and code refactor

--- src/main/java/se/uu/ub/cora/spider/record/SpiderRecordCreatorImp.java
+++ src/main/java/se/uu/ub/cora/spider/record/SpiderRecordCreatorImp.java
@@ -200,11 +200,15 @@ public final class SpiderRecordCreatorImp extends SpiderRecordHandler
 
 	private void ensureIdExists(String recordType) {
 		if (recordTypeHandler.shouldAutoGenerateId()) {
-			SpiderDataGroup recordInfo = recordAsSpiderDataGroup.extractGroup(RECORD_INFO);
-			if(recordInfo.containsChildWithNameInData("id")){
-				recordInfo.removeChild("id");
-			}
+			removeIdIfPresentInData();
 			generateAndAddIdToRecordInfo(recordType);
+		}
+	}
+
+	private void removeIdIfPresentInData() {
+		SpiderDataGroup recordInfo = recordAsSpiderDataGroup.extractGroup(RECORD_INFO);
+		if(recordInfo.containsChildWithNameInData("id")){
+			recordInfo.removeChild("id");
 		}
 	}
 
--- src/test/java/se/uu/ub/cora/spider/spy/RecordStorageDuplicateSpy.java
+++ src/test/java/se/uu/ub/cora/spider/spy/RecordStorageDuplicateSpy.java
@@ -1,66 +1,82 @@
 package se.uu.ub.cora.spider.spy;
 
-import se.uu.ub.cora.bookkeeper.data.DataGroup;
-import se.uu.ub.cora.spider.record.storage.RecordStorage;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import se.uu.ub.cora.bookkeeper.data.DataGroup;
+import se.uu.ub.cora.spider.record.storage.RecordConflictException;
+import se.uu.ub.cora.spider.record.storage.RecordStorage;
+import se.uu.ub.cora.spider.testdata.DataCreator;
+
 public class RecordStorageDuplicateSpy implements RecordStorage {
-    public List<String> requiredIds = new ArrayList<>();
-    @Override
-    public DataGroup read(String type, String id) {
-        return null;
-    }
+	public List<String> requiredIds = new ArrayList<>();
 
-    @Override
-    public void create(String type, String id, DataGroup record, DataGroup collectedTerms, DataGroup linkList, String dataDivider) {
-            requiredIds.add(id);
-    }
+	@Override
+	public DataGroup read(String type, String id) {
+		if (type.equals("recordType")) {
+			DataGroup group = DataCreator.createRecordTypeWithIdAndUserSuppliedIdAndAbstract(id,
+					"true", "false");
+			return group;
+		}
+		return null;
+	}
 
-    @Override
-    public void deleteByTypeAndId(String type, String id) {
+	@Override
+	public void create(String type, String id, DataGroup record, DataGroup collectedTerms,
+			DataGroup linkList, String dataDivider) {
+		if (requiredIds.contains(id)) {
+			throw new RecordConflictException("Record with recordId: " + id + " already exists");
 
-    }
+		}
+		requiredIds.add(id);
 
-    @Override
-    public boolean linksExistForRecord(String type, String id) {
-        return false;
-    }
+	}
 
-    @Override
-    public void update(String type, String id, DataGroup record, DataGroup collectedTerms, DataGroup linkList, String dataDivider) {
+	@Override
+	public void deleteByTypeAndId(String type, String id) {
 
-    }
+	}
 
-    @Override
-    public Collection<DataGroup> readList(String type, DataGroup filter) {
-        return null;
-    }
+	@Override
+	public boolean linksExistForRecord(String type, String id) {
+		return false;
+	}
 
-    @Override
-    public Collection<DataGroup> readAbstractList(String type, DataGroup filter) {
-        return null;
-    }
+	@Override
+	public void update(String type, String id, DataGroup record, DataGroup collectedTerms,
+			DataGroup linkList, String dataDivider) {
 
-    @Override
-    public DataGroup readLinkList(String type, String id) {
-        return null;
-    }
+	}
 
-    @Override
-    public Collection<DataGroup> generateLinkCollectionPointingToRecord(String type, String id) {
-        return null;
-    }
+	@Override
+	public Collection<DataGroup> readList(String type, DataGroup filter) {
+		return null;
+	}
 
-    @Override
-    public boolean recordsExistForRecordType(String type) {
-        return false;
-    }
+	@Override
+	public Collection<DataGroup> readAbstractList(String type, DataGroup filter) {
+		return null;
+	}
 
-    @Override
-    public boolean recordExistsForAbstractOrImplementingRecordTypeAndRecordId(String type, String id) {
-        return false;
-    }
+	@Override
+	public DataGroup readLinkList(String type, String id) {
+		return null;
+	}
+
+	@Override
+	public Collection<DataGroup> generateLinkCollectionPointingToRecord(String type, String id) {
+		return null;
+	}
+
+	@Override
+	public boolean recordsExistForRecordType(String type) {
+		return false;
+	}
+
+	@Override
+	public boolean recordExistsForAbstractOrImplementingRecordTypeAndRecordId(String type,
+			String id) {
+		return false;
+	}
 }
