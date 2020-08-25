fb2ebb0dffc97f2e8db10c7118e32b1d4db94a62
madde <madde@f9809f8f4b71>
2020-08-25 07:14:19 +0000

    Fixed a missing test (discovered by mutation testing)

--- src/main/java/se/uu/ub/cora/spider/record/SpiderRecordCreatorImp.java
+++ src/main/java/se/uu/ub/cora/spider/record/SpiderRecordCreatorImp.java
@@ -199,8 +199,6 @@ public final class SpiderRecordCreatorImp extends SpiderRecordHandler
 
 	private void ensureIdExists(String recordType) {
 		if (recordTypeHandler.shouldAutoGenerateId()) {
-			// TODO: kolla att remove körs. Mutation testing visar att metodanropet kan tas bort
-			// utan att ngt test failar
 			removeIdIfPresentInData();
 			generateAndAddIdToRecordInfo(recordType);
 		}
@@ -208,7 +206,6 @@ public final class SpiderRecordCreatorImp extends SpiderRecordHandler
 
 	private void removeIdIfPresentInData() {
 		DataGroup recordInfo = recordAsDataGroup.getFirstGroupWithNameInData(RECORD_INFO);
-		// TODO: Mutation testing visar att if:en kan negeras utan att ngt test failar
 		if (recordInfo.containsChildWithNameInData("id")) {
 			recordInfo.removeFirstChildWithNameInData("id");
 		}
