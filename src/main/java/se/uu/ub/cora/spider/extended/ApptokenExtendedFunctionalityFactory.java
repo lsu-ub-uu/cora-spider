5d841446f41ed4cdaa9732352b884840f5d6cfbd
madde <madde@74161168ca06>
2020-11-10 06:07:42 +0000

    Started on apptoken extended functionality factory

---
+++ src/main/java/se/uu/ub/cora/spider/extended/ApptokenExtendedFunctionalityFactory.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2020 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.uu.ub.cora.spider.extended;
+
+import static se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionalityPosition.CREATE_BEFORE_METADATA_VALIDATION;
+import static se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionalityPosition.CREATE_BEFORE_RETURN;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import se.uu.ub.cora.spider.dependency.SpiderDependencyProvider;
+import se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionality;
+import se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionalityContext;
+import se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionalityFactory;
+import se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionalityPosition;
+
+public class ApptokenExtendedFunctionalityFactory implements ExtendedFunctionalityFactory {
+
+	private List<ExtendedFunctionalityContext> contexts = new ArrayList<>();
+
+	@Override
+	public void initializeUsingDependencyProvider(SpiderDependencyProvider dependencyProvider) {
+		createListOfContexts();
+
+	}
+
+	private void createListOfContexts() {
+		createContext(CREATE_BEFORE_METADATA_VALIDATION);
+		createContext(CREATE_BEFORE_RETURN);
+	}
+
+	private void createContext(ExtendedFunctionalityPosition position) {
+		contexts.add(new ExtendedFunctionalityContext(position, "appToken", 0));
+	}
+
+	@Override
+	public ExtendedFunctionality factor(ExtendedFunctionalityPosition position, String recordType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ExtendedFunctionalityContext> getExtendedFunctionalityContexts() {
+		return contexts;
+	}
+
+}
