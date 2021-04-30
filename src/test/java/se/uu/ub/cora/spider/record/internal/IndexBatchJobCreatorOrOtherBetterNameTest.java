552c47198c9c0e670fa8d3faaf428570e6ca0533
olovm <olov.mckie@ub.uu.se>
2021-04-30 06:46:26 +0000

    moved to other package

--- src/test/java/se/uu/ub/cora/spider/record/internal/IndexBatchJobCreatorOrOtherBetterNameTest.java
+++ src/test/java/se/uu/ub/cora/spider/record/internal/IndexBatchJobCreatorOrOtherBetterNameTest.java
@@ -16,7 +16,7 @@
  *     You should have received a copy of the GNU General Public License
  *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
  */
-package se.uu.ub.cora.spider.extendedfunctionality.internal;
+package se.uu.ub.cora.spider.record.internal;
 
 import org.testng.annotations.Test;
 
