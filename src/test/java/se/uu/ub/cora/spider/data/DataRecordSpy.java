4a07ef11ca3764d90b6961eaf21a6f72958274bf
olovm <olov.mckie@ub.uu.se>
2022-02-03 10:01:09 +0000

    updating cora-data

--- src/test/java/se/uu/ub/cora/spider/data/DataRecordSpy.java
+++ src/test/java/se/uu/ub/cora/spider/data/DataRecordSpy.java
@@ -139,4 +139,10 @@ public class DataRecordSpy implements DataRecord {
 		return false;
 	}
 
+	@Override
+	public String getSearchId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
