a199e505cfc3bba113619cd49750b030e5db4945
olovm <olov.mckie@ub.uu.se>
2024-06-28 14:15:59 +0000

    adding AppTokenClearTextExtendedFuncionality

---
+++ src/main/java/se/uu/ub/cora/spider/extended/apptoken/AppTokenClearTextExtendedFuncionality.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2024 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.uu.ub.cora.spider.extended.apptoken;
+
+import java.util.List;
+import java.util.Map;
+
+import se.uu.ub.cora.data.DataAtomic;
+import se.uu.ub.cora.data.DataGroup;
+import se.uu.ub.cora.data.DataProvider;
+import se.uu.ub.cora.data.DataRecordLink;
+import se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionality;
+import se.uu.ub.cora.spider.extendedfunctionality.ExtendedFunctionalityData;
+
+public class AppTokenClearTextExtendedFuncionality implements ExtendedFunctionality {
+	private Map<String, String> efSystemSecretIdAndClearTextToken;
+
+	@Override
+	public void useExtendedFunctionality(ExtendedFunctionalityData data) {
+		Map<String, Object> dataSharer = data.dataSharer;
+		efSystemSecretIdAndClearTextToken = (Map<String, String>) dataSharer
+				.get("AppTokenHandlerExtendedFunctionality");
+
+		possiblyAddClearTextsToAppTokens(data.dataGroup);
+	}
+
+	private void possiblyAddClearTextsToAppTokens(DataGroup dataGroup) {
+		if (clearTextsExists()) {
+			addClearTextsToAppTokens(dataGroup);
+		}
+	}
+
+	private boolean clearTextsExists() {
+		return !efSystemSecretIdAndClearTextToken.isEmpty();
+	}
+
+	private void addClearTextsToAppTokens(DataGroup dataGroup) {
+		List<DataGroup> appTokenGroups = getListOfAppTokenGroups(dataGroup);
+		for (DataGroup appTokenGroup : appTokenGroups) {
+			possiblySetClearTextInAppTokenGroup(appTokenGroup);
+		}
+	}
+
+	private List<DataGroup> getListOfAppTokenGroups(DataGroup dataGroup) {
+		DataGroup appTokensGroup = dataGroup.getFirstGroupWithNameInData("appTokens");
+		return appTokensGroup.getChildrenOfTypeAndName(DataGroup.class, "appToken");
+	}
+
+	private void possiblySetClearTextInAppTokenGroup(DataGroup appTokenGroup) {
+		String systemSecretId = getSystemSecretId(appTokenGroup);
+		if (efSystemSecretIdAndClearTextToken.containsKey(systemSecretId)) {
+			String clearText = efSystemSecretIdAndClearTextToken.get(systemSecretId);
+			setClearTextInAppTokenGroup(appTokenGroup, clearText);
+		}
+	}
+
+	private String getSystemSecretId(DataGroup appTokenGroup) {
+		DataRecordLink link = appTokenGroup.getFirstChildOfTypeAndName(DataRecordLink.class,
+				"appTokenLink");
+		return link.getLinkedRecordId();
+	}
+
+	private void setClearTextInAppTokenGroup(DataGroup appTokenGroup, String clearText) {
+		DataAtomic clearTextAtomic = DataProvider
+				.createAtomicUsingNameInDataAndValue("appTokenClearText", clearText);
+		appTokenGroup.addChild(clearTextAtomic);
+	}
+
+}
