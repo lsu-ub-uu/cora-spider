4600b9d5ac3f8c58a472ed69c8881688e87f603a
olovm <olov.mckie@ub.uu.se>
2025-11-17 14:06:18 +0000

    removing collectedLinks if in trash, no incoming links when setting to in trash

--- src/main/java/se/uu/ub/cora/spider/record/internal/RecordUpdaterImp.java
+++ src/main/java/se/uu/ub/cora/spider/record/internal/RecordUpdaterImp.java
@@ -167,7 +167,7 @@ public final class RecordUpdaterImp extends RecordHandler implements RecordUpdat
 		// TODO: isInTrashBin no collected links think of how to handle unique but still beeing able
 		// to filter, add extra check in unique?
 		// no collected links, DONE
-		// unique left to fix
+		// unique left to fix, somehow
 		if (isInTrashBin()) {
 			collectedLinks = Collections.emptySet();
 		}
